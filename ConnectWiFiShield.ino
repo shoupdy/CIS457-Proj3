d118fbf6ebf825e397309399bffa12f55ff3f20b
scheffal <scheffal@mail.gvsu.edu>
2018-12-02 13:13:35 -0500

    Arduino/ESP8266 code

---
+++ ConnectWiFiShield.ino
@@ -0,0 +1,154 @@
+/*************************************************
+ * CIS 457: ESP8266 WiFi
+ * Ali Scheffler
+ * Dylan Shoup
+ * 12/4/18
+ * 
+ * This program reads in values from serial, and 
+ * updates Firebase database with the new data.
+ */
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <SoftwareSerial.h>
+#include <string.h>
+
+//Password for hotspot to connect to 
+//const char* ssid = "NETGEAR81";
+//const char* password = "ancientcomet701";
+const char* ssid = "ATT4SUsuI2";
+const char* password = "ct?uyuztadd3";
+
+//Information to connect to Firebase
+#define FIREBASE_HOST "nodemcu-ac90a.firebaseio.com"
+#define FIREBASE_AUTH "MCrHpYT3hZPbFZwrCsKuneDdlcBHmPhGi4RbUVxf"
+
+//For reading data from serial
+String inString = "";
+
+//Pass key
+String token = "keyword";
+
+void read_data();
+bool wait_password();
+
+void setup() {
+      
+      Serial.begin(115200);
+    
+      //Print out data about connecting 
+      Serial.println();
+      Serial.println();
+      Serial.print("You're are connecting to ");
+      Serial.println(ssid);
+
+      //Connect to WiFi
+      WiFi.begin(ssid, password);
+
+      //Wait while not connected to a network
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        Serial.println("");
+        
+      }
+      Serial.println("Your ESP is connected!");  
+      Serial.println("Your IP address is: ");
+      Serial.println(WiFi.localIP()); 
+
+      //Start Firebase
+      Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop() {
+      bool secure = false;
+      
+      //Wait for password
+      while(!secure)
+      {
+        secure = wait_password();
+      }
+      //Read in from serial
+      read_data();
+}
+
+bool wait_password()
+{
+  while(Serial.available() > 0)
+  {
+    int data = Serial.read();
+    
+    if(data == '\r')
+    {
+      inString = inString; 
+      if(inString.equals(token))
+      {
+        return true;
+      }
+      
+    }
+    inString += (char) data;
+    
+    delay(2000);
+  }
+
+  inString = "";
+  
+  return false;
+}
+
+void read_data()
+{
+  int counter = 0; 
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  
+  int temperature = 0;
+  int humidity = 0;
+  
+  while(Serial.available() > 0)
+  {
+    int data = Serial.read();  
+
+    if(isDigit(data))
+    {
+      inString += (char) data;
+    }
+
+    if(data == '\n')
+    {
+      if(counter == 0)
+      {
+        temperature = inString.toInt();
+  
+        counter++;
+      }else{
+        humidity = inString.toInt();
+        
+        counter = 0;
+      }
+      
+      //clear the string for new input 
+      inString = "";
+    }
+  
+  }
+
+  if(temperature > 0 && temperature < 100 && humidity > 0 & humidity < 100)
+  {
+    root["temperature"] = temperature;
+    root["humidity"] = humidity;
+    
+    String name = Firebase.push("/sensor/values", root);
+
+    if(Firebase.failed())
+    {
+        Serial.println("Firebase Pushing /sensor/dht failed:");
+        Serial.println(Firebase.error());
+    }
+  }
+    
+
+  delay(5000);
+
+}
---
+++ temp_hum.ino
@@ -0,0 +1,60 @@
+/***********************************************************8
+ * CIS 457: Temperature and Humidity Reader
+ * Ali Scheffler
+ * Dylan Shoup
+ * 12/4/18
+ * 
+ * This program reads the temperature and humidity from the DHT 
+ * sensor, then sends the values over serial communcation to 
+ * the ESP8266
+ */
+ 
+#include <dht.h>
+#include <SoftwareSerial.h>
+#include <string.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+#define RXpin 10
+#define TXpin 11
+
+//Password to send to ESP8266
+String token = "keyword";
+
+//Setup serial communication
+SoftwareSerial espSerial(RXpin, TXpin);
+
+void setup() {
+  //Serial for outputting to monitor
+  Serial.begin(9600);
+
+  //Serial for sending data
+  espSerial.begin(115200);
+
+  //Send token 
+  espSerial.println(token);
+}
+
+void loop() {
+  // Read sensor
+  int chk = DHT.read11(DHT11_PIN);
+
+  //Clear serial 
+  espSerial.flush();
+  
+  //Send data to ESP8266 via serial
+  int temp = DHT.temperature;
+  Serial.print("Temp: ");
+  Serial.println(temp);
+  espSerial.println(temp);
+ 
+  int hum = DHT.humidity;
+  Serial.print("Hum: ");
+  Serial.println(hum);
+  espSerial.println(hum);
+  Serial.println();
+
+  //Delay so firebase is getting updated every half a minute
+  delay(30000);
+}
